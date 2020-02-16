e34bf3ea034bc8208adb83a3b30a0d1f32776dae
Basil <vogurt007@gmail.com>
2020-02-16 13:49:15 +0300

    comitti

--- homework.py
+++ homework.py
@@ -1,7 +1,7 @@
 import datetime as dt
 
 class Record:
-     def __init__(self, amount, comment, date = ''):
+     def __init__(self, amount, comment, date=None):
           date_format = '%d.%m.%Y'
           self.amount = amount
           self.date =  dt.datetime.strptime(date, date_format).date() if date else dt.date.today()
@@ -16,48 +16,42 @@ class Calculator:
           self.records.append(Record)
 
      def get_today_stats(self):
-          sumDay = 0
-          datenow = dt.datetime.now().date()
-          for Notes in self.records:
-               if Notes.date == datenow:
-                   sumDay = sumDay + int(Notes.amount)    
-          return sumDay
+          sum_day = 0
+          date_now = dt.datetime.now().date()
+          for notes in self.records:
+               if notes.date == date_now:
+                   sum_day = sum_day + int(notes.amount)    
+          return sum_day
      def get_week_stats(self):
-          sumWeek = 0
-          datenow = dt.datetime.now().date()
-          timedelta = datenow - dt.timedelta(7)
-          for Notes in self.records:
-               if timedelta <= Notes.date <= datenow:
-                    sumWeek = sumWeek + int(Notes.amount)
-          return sumWeek
+          sum_week = 0
+          date_now = dt.datetime.now().date()
+          timedelta = date_now - dt.timedelta(6)
+          for notes in self.records:
+               if timedelta <= notes.date <= date_now:
+                    sum_week = sum_week + int(notes.amount)
+          return sum_week
 
 class CashCalculator(Calculator):
      USD_RATE = 63.0
      EURO_RATE = 68.7
-     N = 1.0
+    
                
      def __init__(self, limit):
           super().__init__(limit)
 
-     def get_today_cash_remained(self, currency):
+     def get_today_cash_remained(self, currency=None):
           total = self.get_today_stats()
-          if currency == 'usd':
-               cur = 'USD'
-               money = round((self.limit - total)/self.USD_RATE,2)
-          elif currency == 'eur':
-               cur = 'Euro'
-               money = round((self.limit - total)/self.EURO_RATE,2)
-          else:
-               cur = 'руб'
-               money = round((self.limit - total)/self.N,2)
+          exchange = {"rub": 1.0, "usd": self.USD_RATE, "eur": self.EURO_RATE} 
+          valuta = {"rub": 'руб', "usd": 'USD', "eur": 'Euro'}
+          money = round((self.limit - total)/exchange[currency],2)         
           if total < self.limit:     
-               answer = ('На сегодня осталось ' + str(money) + ' ' + cur)
+               answer = F'На сегодня осталось {money} {valuta[currency]}'
           elif total == self.limit: 
                answer = 'Денег нет, держись'         
           else:
                if money < 0:
-                    money = money * (-1)
-               answer = ('Денег нет, держись: твой долг - '+ str(money) + ' ' + cur)     
+                    money = money*(-1)
+                    answer = F'Денег нет, держись: твой долг - {money} {valuta[currency]}'     
           return answer
 
 
@@ -66,9 +60,9 @@ class CaloriesCalculator(Calculator):
           super().__init__(limit)
           
      def get_calories_remained(self):
-          LimitCalor = self.limit - self.get_today_stats()
-          if LimitCalor > 0 :
-               answer = ('Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более ' + str(LimitCalor) + ' кКал') 
+          lim_calories = self.limit - self.get_today_stats()
+          if lim_calories > 0 :
+               answer = F'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {lim_calories} кКал'
           else:
                answer = 'Хватит есть!'
           return answer           
