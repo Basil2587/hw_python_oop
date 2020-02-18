518ec3ffc701f1775ac8962cd92689ffcc71619a
Basil <vogurt007@gmail.com>
2020-02-18 12:48:15 +0300

    project

--- homework.py
+++ homework.py
@@ -20,6 +20,7 @@ class Calculator:
           date_now = dt.datetime.now().date()
           sum_day = sum([notes.amount for notes in self.records if notes.date == date_now])      
           return sum_day
+
      def get_week_stats(self):
           date_now = dt.datetime.now().date()
           timedelta = date_now - dt.timedelta(6)
@@ -30,33 +31,31 @@ class CashCalculator(Calculator):
      USD_RATE = 63.0
      EURO_RATE = 68.7
                   
-     def __init__(self, limit):
-          super().__init__(limit)
-
      def get_today_cash_remained(self, currency=None):
           total = self.get_today_stats()
-          exchange = {"rub": 1.0, "usd": self.USD_RATE, "eur": self.EURO_RATE} 
-          valuta = {"rub": 'руб', "usd": 'USD', "eur": 'Euro'}
-          money = round((self.limit - total)/exchange[currency],2)         
-          if total < self.limit:     
-               answer = F'На сегодня осталось {money} {valuta[currency]}'
+          exchange = {'rub': (1.0, 'руб'),
+                      'usd': (self.USD_RATE, 'USD'),
+                      'eur': (self.EURO_RATE, 'Euro'),
+          }
+          a, b = exchange[currency] 
+          money = round((self.limit - total)/a,2)
+          if total < self.limit:
+               answer = F'На сегодня осталось {money} {b}'
           elif total == self.limit: 
-               answer = 'Денег нет, держись'         
+               answer = 'Денег нет, держись'
           else:
                if money < 0:
-                    money = money*(-1)
-                    answer = F'Денег нет, держись: твой долг - {money} {valuta[currency]}'     
+                    money = abs(money)
+                    answer = F'Денег нет, держись: твой долг - {money} {b}'     
           return answer
 
 
 class CaloriesCalculator(Calculator):
-     def __init__(self, limit):
-          super().__init__(limit)
           
      def get_calories_remained(self):
           lim_calories = self.limit - self.get_today_stats()
-          if lim_calories > 0 :
+          if lim_calories > 0:
                answer = F'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {lim_calories} кКал'
           else:
                answer = 'Хватит есть!'
-          return answer           
+          return answer
